04e5ac125235d48d21c95e427712c94c3536ce8b
Jeff Chern <jchern@yahoo-inc.com>
2014-07-03 20:51:13 -0700

    Hangman generates empty spaces

---
+++ hangman/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+string pickWord()
+{
+    // TODO: Actually randomize later.
+    return "WE'RE TRYING TO CODE HANGMAN";
+}
+
+/*
+ * Converts a string of input words into a string of underscores,
+ * where each letter in the input string becomes an underscore, and
+ * spaces remain spaces.
+ *
+ * "THIS IS SOME SECRET"
+ * "____ __ ____ ______"
+ */
+string makeUnderscores(string input)
+{
+    string output;
+    for (unsigned int i = 0; i < input.length(); i++)
+    {
+        char c = input[i];
+        if (c >= 'A' && c <= 'Z')
+        {
+            output += "_ ";
+        }
+        else
+        {
+            output += c;
+            output += ' ';
+        }
+    }
+
+    return output;
+}
+
+void playGame(string secret)
+{
+    string progress = makeUnderscores(secret);
+    cout << progress << endl;
+}
+
+int main()
+{
+    string word = pickWord();
+    playGame(word);
+    return 0;
+}
