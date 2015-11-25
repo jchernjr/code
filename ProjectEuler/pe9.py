8ad3a058bb69b51f9a3032d82166354ddffc7bfd
Jeff Chern <jchern@yahoo-inc.com>
2015-11-24 16:13:36 -0800

    Project Euler: #9 solved

---
+++ ProjectEuler/pe9.py
@@ -0,0 +1,30 @@
+
+# Brute force search for a Pythagorean triplet (a^2 + b^2 = c^2)
+
+def isPythagTriplet(a, b, c):
+  return (a*a + b*b == c*c)
+
+def makeSquares(k1, k2):
+  """
+  Return a set of square numbers, [k1^2, ... , k2^2].
+  I use a set instead of a list, so that it's easy to check if a number is a square in O(1) time.
+  """
+  return set([k*k for k in xrange(k1, k2 + 1)])
+
+def main():
+  TARGET_SUM = 1000
+
+  KNOWN_SQUARES = makeSquares(1, TARGET_SUM)
+ 
+  # Check all pairs of integers (a,b) 
+  # To avoid checking duplicates (a,b) (b,a), enforce a <= b <= c.
+  for a in xrange(1, TARGET_SUM):
+    for b in xrange(a, TARGET_SUM):
+      # See if this choice of (a,b) leaves enough room for a large enough c.
+      c = TARGET_SUM - a - b
+      if c < a or c < b:
+        continue
+
+      # See if this choice of (a,b) yields a valid hypotenuse c.
+      if isPythagTriplet(a, b, c):
+        print a, b, c
